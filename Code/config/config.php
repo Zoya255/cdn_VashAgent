bd6292bf3949e27287815d34b3293a8faf42b0c7
Cet <korlyakov.2001@mail.ru>
2020-12-04 11:49:08 +1000

    update code, integrated update of EasySpy V2

--- Code/config/config.php
+++ Code/config/config.php
@@ -5,3 +5,4 @@
 	$CONF_DB_WORK = "u1212385_vashagent_orderdb";
 	$CONF_USER    = "u1212385_default";
 	$CONF_PASS    = "11iAoy_!11iAoy_!";
+	$APIKEY       = "a7b68168661077a428d006a605de4ba06ef9d688";
