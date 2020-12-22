9b4c8140c910cdefa8bd8585d924187ca0871c9d
Cet <korlyakov.2001@mail.ru>
2020-12-22 20:31:12 +1000

    postfix: update telephone form

--- Code/js/page-index.js
+++ Code/js/page-index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	/* настройка переключения слайдов */
 
 	$('#script__pagepiling').pagepiling({
 		verticalCentered : false,
@@ -7,32 +8,86 @@ $(document).ready(function() {
 		menu             : '#script__pagepiling-menu'
 	});
 
+	/* обработка формы */
+
 	let status = false;
 
 	$('#script__form-button').click(function () {
+		let errors = 0;
 
-		if (!status) {
-			$.ajax({
-				method   : "POST",
-				url      : "php/form.php",
-				data     : {
-					name      : $('#script__form-name').val(),
-					telephone : $('#script__form-tel').val(),
-					email     : $('#script__form-email').val(),
-				},
-				success  : function (data, textStatus) {
-					console.log(textStatus);
-					$('#script__form-button').val("Отправлено");
-					status = true;
-				},
-				error    : function (jqXHR, textStatus) {
-					console.error(textStatus);
-					$('#script__form-button').val("Ошибка, попробуйте позже");
-					status = false;
-				},
-			})
+		let name  = $('#script__form-name').val();
+		let tel   = $('#script__form-tel').val();
+		let email = $('#script__form-email').val();
+
+		if ( name.length < 4 ) {
+			errors++;
+			console.error("Имя: cлишком короткое");
+		}
+		else if ( name.length > 80 ) {
+			errors++;
+			console.error("Имя: cлишком длинное");
+		}
+		else {
+			console.log("Имя: ОК");
 		}
 
+		let phoneNumber = new libphonenumber.AsYouType('RU').input(tel);
+
+		if ( phoneNumber.length === 17 ) {
+			console.log( "Телефон: ОК" );
+			console.log( phoneNumber );
+		}
+		else {
+			errors++;
+			console.error("Телефон: не правильный формат");
+		}
+
+		if ( email.length < 4 ) {
+			errors++;
+			console.error("Эл. почта: Слишком короткая");
+		}
+		if ( email.length > 80 ) {
+			errors++;
+			console.error("Эл. почта: Слишком длинная");
+		}
+		else {
+			console.log("Эл. почта: ОК");
+		}
+
+		if ( errors === 0 ) {
+			if (!status) {
+				$.ajax({
+					method   : "POST",
+					url      : "php/form.php",
+					data     : {
+						name      : name,
+						telephone : tel,
+						email     : email,
+					},
+					success  : function (data, textStatus) {
+						console.log(textStatus);
+						$('#script__form-button').val("Отправлено");
+						status = true;
+					},
+					error    : function (jqXHR, textStatus) {
+						console.error(textStatus);
+						$('#script__form-button').val("Ошибка, попробуйте позже");
+						status = false;
+					},
+				})
+			}
+		}
+
+	})
+
+	/* обработка номера телефона на форме */
+
+	$(".global__section-forms-form").keyup( function () {
+		let tel = $('#script__form-tel').val();
+		let phoneNumber = new libphonenumber.AsYouType("RU").input(tel);
+
+		$('#script__form-tel').val( phoneNumber );
+		console.log( phoneNumber );
 	})
 
 });
